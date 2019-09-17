d6fb5e455c52c381f94337b43be30f97fb11fbc0
pabloSalva <pablo.salvador90@gmail.com>
2019-09-17 20:51:29 -0300

    gitignore

--- consumo/views.py
+++ consumo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,Group
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
